c840cfd800109abff9233afe848c2391068307c8
shiratori-sh <janpaku.games@gmail.com>
2019-03-23 18:17:43 +0900

    インクルメンタルサーチ
    グループメンバー検索機能実装。

---
+++ app/assets/javascripts/add_user.js
@@ -0,0 +1,51 @@
+$(function(){
+  var user_list = $("#user-search-result");
+
+  function appendUsers(user) {
+    var html =`<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">
+                ${user.name}
+                </p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id} " data-user-name=" ${user.name }">追加
+                </a>
+                </div>`
+    user_list.append(html);
+  }
+
+function appendUsersErrorHTML(error){
+  var html =`<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ error }</p>
+                </div>`
+    user_list.append(html)
+}
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+      if (input.length != 0){
+        $.ajax({
+          type: 'GET',
+          url: '/users',
+          data: { keyword: input },
+          dataType: 'json'
+        })
+
+      .done(function(users) {
+        $("#user-search-result").empty();
+          if (users.length !== 0) {
+            users.forEach(function(user){
+              appendUsers(user);
+            })
+          }
+          else{
+            appendUsersErrorHTML("一致するユーザーは見つかりません");
+          }
+        })
+      .fail(function() {
+       alert('ユーザー検索に失敗しました');
+      });
+    }
+      else{
+        $("#user-search-result").empty()
+      }
+  });
+});
